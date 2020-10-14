e7bb8e31db5ba9681a9634e89b5782f4b2940bb1
cyhhenry <cyhhenry@users.noreply.github.com>
2020-10-14 18:54:57 +0200

    created 3 Pokémon objects in pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-alert('Hello world');
-let favoriteFood = 'curry';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Charmeleon',
+        height: 3.07,
+        weight: 41.9,
+        type: 'fire'
+        category: ['flame'],
+        abilities: ['blaze'],
+        weaknesses: ['water', 'ground', 'rock'],
+    },
+    {
+        name: 'Ivysaur',
+        height: 3.03,
+        weight: 28.7,
+        type: ['grass', 'poison']
+        category: 'seed',
+        abilities: ['overgrow'],
+    },
+    {
+        name: 'Wartortle',
+        height: 3.03,
+        weight: 49.6,
+        type: 'water'
+        category: 'turtle',
+        abilities: ['turrent']
+        weaknesses: ['grass', 'electric'],
+    }
+]
