9051cb8cb039ef40885fe40cf3fa506dbcebd09e
cyhhenry <cyhhenry@users.noreply.github.com>
2020-12-20 17:10:42 +0100

    connected .js to external API that fetches Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,6 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Charmeleon',
-            pokedexNumber: 1,
-            height: 5,
-            weight: 41.9,
-            category: 'flame',
-            types: ['fire'],
-            abilities: ['blaze'],
-            weaknesses: ['water', 'ground', 'rock'],
-        },
-        {
-            name: 'Ivysaur',
-            pokedexNumber: 2,
-            height: 3,
-            weight: 28.7,
-            category: 'seed',
-            types: ['grass', 'poison'],
-            abilities: ['overgrow'],
-            weaknesses: ['fire', 'psychic', 'flying', 'ice']
-        },
-        {
-            name: 'Wartortle',
-            pokedexNumber: 3,
-            height: 4,
-            weight: 49.6,
-            category: 'turtle',
-            types: ['water'],
-            abilities: ['turrent'],
-            weaknesses: ['grass', 'electric'],
-        }
-    ]
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
